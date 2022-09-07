2011e34411d54303f4c73805874af6cebc5d38ef
ShelbyW <76605025+ShelbyWong@users.noreply.github.com>
2022-09-07 17:47:08 +0800

    ps0

--- MySolutions/ps0-template/count_long_subarray.py
+++ MySolutions/ps0-template/count_long_subarray.py
@@ -6,5 +6,18 @@ def count_long_subarray(A):
     count = 0
     ##################
     # YOUR CODE HERE #
+    n = len(A)
+    temp_len = 1  # record the length of current subarray
+    length = 1  # record the longest length of increasing subarrays
+    for i in range(1, n):
+        if A[i - 1] < A[i]:
+            temp_len += 1
+        else:
+            temp_len = 1
+        if temp_len == length:
+            count += 1
+        elif temp_len > length:
+            count = 1  # update the number
+            length = temp_len  # store the longest len
     ##################
     return count
